5ece0ce3af61435523dc548a23aa7b166b09d850
Lucas Walker <jose.batista09322@alunos.ufersa.edu.br>
2023-07-09 17:19:01 -0300

    commit

--- IntroJava/Questão11/Coordenadas.java
+++ IntroJava/Questão11/Coordenadas.java
@@ -1,3 +1,7 @@
+// Escreva um programa que lê dois pontos no plano cartesiano com coordenadas
+// x e y e informa se o segundo ponto está acima, abaixo, à esquerda e/ou à direita
+// do primeiro
+
 package Questão11;
 
 import java.util.Scanner;
