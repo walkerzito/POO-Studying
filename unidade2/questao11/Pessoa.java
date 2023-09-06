ce1db939058df7c9860969b743cdbb38c9570149
Lucas Walker <jose.batista09322@alunos.ufersa.edu.br>
2023-09-06 11:28:01 -0300

    estudando

---
+++ unidade2/questao11/Pessoa.java
@@ -0,0 +1,11 @@
+package unidade2.questao11;
+
+public class Pessoa {
+    private String nome;
+    private int idade;
+    
+    public Pessoa(String nome, int idade){
+        this.nome = nome;
+        this.idade = idade;
+    }
+}
---
+++ unidade2/questao11/Politico.java
@@ -0,0 +1,9 @@
+package unidade2.questao11;
+
+public class Politico extends Pessoa{
+    private String partido;
+
+    public Politico(String nome, int idade, String partido){
+        super(nome, idade);
+    }
+}
